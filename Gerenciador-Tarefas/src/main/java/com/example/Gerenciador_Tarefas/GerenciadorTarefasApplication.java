30dd5e967b139bf14a397ef4fa439395d93efb9f
RaphaJimmyLuiz <61025408+RaphaJimmyLuiz@users.noreply.github.com>
2024-06-05 14:15:16 -0300

    Add files via upload

---
+++ Gerenciador-Tarefas/src/main/java/com/example/Gerenciador_Tarefas/GerenciadorTarefasApplication.java
@@ -0,0 +1,14 @@
+package com.example.Gerenciador_Tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GerenciadorTarefasApplication.class, args);
+	}
+
+}
---
+++ Gerenciador-Tarefas/src/main/java/com/example/Gerenciador_Tarefas/repository/TarefaRepository.java
@@ -0,0 +1,8 @@
+package com.example.Gerenciador_Tarefas.repository;
+
+import com.example.Gerenciador_Tarefas.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+}
