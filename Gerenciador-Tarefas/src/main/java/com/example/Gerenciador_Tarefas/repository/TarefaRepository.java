b4ae01c0856cad19688c68ac9e67cd5f14fb23a2
RaphaJimmyLuiz <61025408+RaphaJimmyLuiz@users.noreply.github.com>
2024-06-12 16:27:59 -0300

    Delete Gerenciador-Tarefas directory

--- Gerenciador-Tarefas/src/main/java/com/example/Gerenciador_Tarefas/repository/TarefaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Gerenciador_Tarefas.repository;
-
-import com.example.Gerenciador_Tarefas.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-
-}
